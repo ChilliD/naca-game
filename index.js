a69023ae1f3d353d90eefbd8c0a3779702679a6d
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-10-25 22:44:33 -0400

    update index.js

--- index.js
+++ index.js
@@ -50,4 +50,4 @@ io.on('connection', (client) => {
     });
 });
 
-server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
